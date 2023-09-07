ccebb3c0df0b882dc12348058781e6aca34d0027
RSagar235 <124336584+RSagar235@users.noreply.github.com>
2023-09-07 22:58:55 +0530

    CICD practice

---
+++ src/test/java/com/example/CICDPractice.java
@@ -0,0 +1,15 @@
+package com.example;
+import org.testng.annotations.Test;
+
+public class CICDPractice {
+
+	@Test
+	public void method1() {
+		System.out.println("CICD practice from method1");
+	}
+
+	@Test
+	public void method2() {
+		System.out.println("CICD practice from method2");
+	}
+}
